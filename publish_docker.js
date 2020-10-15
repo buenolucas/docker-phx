2ec637df64307de56118e72ea22c306219a23eab
Lucas Bueno <buenolucasbr@gmail.com>
2020-10-15 15:15:06 -0300

    xxx

--- publish_docker.js
+++ publish_docker.js
@@ -2,7 +2,7 @@ const execSync = require("child_process").execSync;
 
 function publishDocker() {
   const now = new Date()
-  const repo = `buenolucasbr/phx`
+  const repo = `buenolucasbr/cimg-phx`
   const finaltag = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
   const cmd =`
 docker login -u buenolucasbr -p navehub1010
